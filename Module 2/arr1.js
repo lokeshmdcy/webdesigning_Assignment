d8413f1ab5b297efc87531383f2d8ac6841d0057
lokeshmdcy <lokeshmd637@gmail.com>
2023-07-05 10:15:51 +0530

    1st project

---
+++ Module 2/arr1.js
@@ -0,0 +1,9 @@
+let nos = [1 ,2 ,3 ,4 ,5 ,6]
+console.log("Initial All nos:" ,nos);
+console.log("Length of the array is:" ,nos.length);
+console.log("First element is:" ,nos[0]);
+console.log("Last element is:" ,nos[nos.length-1]);
+nos.push(7);
+console.log("After push, All nos:" ,nos);
+nos.pop()
+console.log("After pop, All nos:" ,nos);
